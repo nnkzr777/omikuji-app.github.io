0784daf7a4805009f6a229d643b833f322b52698
nanapi <71760921+nnkzr777@users.noreply.github.com>
2022-05-30 11:59:18 +0900

    Delete lesson10/lesson10_1 directory

--- lesson10/lesson10_1/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', mount);
-
-function mount() {
-    let count = 0;
-    setInterval(() => {
-        count++;
-        document.getElementById('timer').innerText = count.toString();
-    }, 1000);
-}
